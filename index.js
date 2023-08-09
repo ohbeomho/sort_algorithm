d7e78e4bd29fb4139cad8546bb8e7fe6a202f455
OhBeomho <tyler0322@naver.com>
2023-08-09 19:49:18 +0900

    tab size

--- index.js
+++ index.js
@@ -11,231 +11,231 @@ const numbers = [];
 const bars = [];
 
 elementsInput.addEventListener("change", () => {
-  if (elementsInput.valueAsNumber > 200) {
-    elements = 200;
-    elementsInput.value = "200";
-  } else elements = elementsInput.valueAsNumber;
+	if (elementsInput.valueAsNumber > 200) {
+		elements = 200;
+		elementsInput.value = "200";
+	} else elements = elementsInput.valueAsNumber;
 
-  shuffle();
+	shuffle();
 });
 speedInput.addEventListener("change", () => {
-  if (speed > 10) {
-    speed = 10;
-    speedInput.value = "10";
-  } else speed = speedInput.valueAsNumber;
+	if (speed > 10) {
+		speed = 10;
+		speedInput.value = "10";
+	} else speed = speedInput.valueAsNumber;
 });
 algorithmSelect.addEventListener(
-  "change",
-  () => (algorithm = algorithmSelect.selectedOptions[0].value)
+	"change",
+	() => (algorithm = algorithmSelect.selectedOptions[0].value)
 );
 
 let step = 0;
 let playing = false;
 
 function generateBars() {
-  if (bars.length) {
-    bars.splice(0, bars.length);
-    Array.from(document.querySelectorAll(".bar")).forEach((bar) => bar.remove());
-  }
+	if (bars.length) {
+		bars.splice(0, bars.length);
+		Array.from(document.querySelectorAll(".bar")).forEach((bar) => bar.remove());
+	}
 
-  for (let number of numbers) {
-    const bar = document.createElement("div");
-    bar.classList.add("bar");
-    bar.style.flex = 1;
-    bar.style.height = `${(number / elements) * 100}%`;
+	for (let number of numbers) {
+		const bar = document.createElement("div");
+		bar.classList.add("bar");
+		bar.style.flex = 1;
+		bar.style.height = `${(number / elements) * 100}%`;
 
-    document.body.appendChild(bar);
-    bars.push(bar);
-  }
+		document.body.appendChild(bar);
+		bars.push(bar);
+	}
 }
 
 function generateNumbers() {
-  if (numbers.length) numbers.splice(0, numbers.length);
+	if (numbers.length) numbers.splice(0, numbers.length);
 
-  const temp = [];
-  for (let i = 0; i < elements; i++) temp.push(i + 1);
+	const temp = [];
+	for (let i = 0; i < elements; i++) temp.push(i + 1);
 
-  // Shuffle
-  while (temp.length > 0) {
-    const removed = temp.splice(Math.floor(Math.random() * temp.length), 1)[0];
-    numbers.push(removed);
-  }
+	// Shuffle
+	while (temp.length > 0) {
+		const removed = temp.splice(Math.floor(Math.random() * temp.length), 1)[0];
+		numbers.push(removed);
+	}
 }
 
 function highlightBar(i) {
-  bars[i].classList.add("highlight");
+	bars[i].classList.add("highlight");
 }
 
 function makeButton(type) {
-  const button = document.createElement("button");
-  button.innerText = type.charAt(0).toUpperCase() + type.substring(1);
+	const button = document.createElement("button");
+	button.innerText = type.charAt(0).toUpperCase() + type.substring(1);
 
-  const event =
-    type === "play"
-      ? play
-      : type === "stop"
-        ? stop
-        : type === "resume"
-          ? resume
-          : type === "pause"
-            ? pause
-            : shuffle;
-  button.addEventListener("click", event);
+	const event =
+		type === "play"
+			? play
+			: type === "stop"
+			? stop
+			: type === "resume"
+			? resume
+			: type === "pause"
+			? pause
+			: shuffle;
+	button.addEventListener("click", event);
 
-  buttons.appendChild(button);
+	buttons.appendChild(button);
 }
 
 function removeButtons() {
-  buttons.innerHTML = "";
+	buttons.innerHTML = "";
 }
 
 function shuffle() {
-  generateNumbers();
-  generateBars();
+	generateNumbers();
+	generateBars();
 }
 
 function play() {
-  if (step) {
-    step = 0;
-    shuffle();
-  }
+	if (step) {
+		step = 0;
+		shuffle();
+	}
 
-  elementsInput.disabled = true;
-  speedInput.disabled = true;
-  algorithmSelect.disabled = true;
-  playing = true;
+	elementsInput.disabled = true;
+	speedInput.disabled = true;
+	algorithmSelect.disabled = true;
+	playing = true;
 
-  removeButtons();
-  makeButton("pause");
-  makeButton("stop");
+	removeButtons();
+	makeButton("pause");
+	makeButton("stop");
 
-  sort();
+	sort();
 }
 
 function pause() {
-  playing = false;
-  speedInput.disabled = false;
+	playing = false;
+	speedInput.disabled = false;
 
-  removeButtons();
-  makeButton("resume");
+	removeButtons();
+	makeButton("resume");
 }
 
 function resume() {
-  playing = true;
-  speedInput.disabled = true;
+	playing = true;
+	speedInput.disabled = true;
 
-  removeButtons();
-  makeButton("pause");
-  makeButton("stop");
+	removeButtons();
+	makeButton("pause");
+	makeButton("stop");
 
-  sort();
+	sort();
 }
 
 function stop() {
-  elementsInput.disabled = false;
-  speedInput.disabled = false;
-  algorithmSelect.disabled = false;
-  step = 0;
-  playing = false;
+	elementsInput.disabled = false;
+	speedInput.disabled = false;
+	algorithmSelect.disabled = false;
+	step = 0;
+	playing = false;
 
-  removeButtons();
-  makeButton("play");
-  makeButton("shuffle");
+	removeButtons();
+	makeButton("play");
+	makeButton("shuffle");
 }
 
 async function sort() {
-  let s = step;
+	let s = step;
 
-  if (algorithm === "selection") {
-    for (let i = 0; i < elements; i++) {
-      for (let j = i + 1; j < elements; j++) {
-        if (s) {
-          s--;
-          continue;
-        }
+	if (algorithm === "selection") {
+		for (let i = 0; i < elements; i++) {
+			for (let j = i + 1; j < elements; j++) {
+				if (s) {
+					s--;
+					continue;
+				}
 
-        if (!playing) return;
+				if (!playing) return;
 
-        step++;
+				step++;
 
-        if (numbers[i] > numbers[j]) {
-          const temp = numbers[i];
-          numbers[i] = numbers[j];
-          numbers[j] = temp;
+				if (numbers[i] > numbers[j]) {
+					const temp = numbers[i];
+					numbers[i] = numbers[j];
+					numbers[j] = temp;
 
-          generateBars();
-          highlightBar(i);
-          highlightBar(j);
-          await delay();
-        }
-      }
-    }
-  } else if (algorithm === "bubble") {
-    for (let i = 0; i < elements; i++) {
-      for (let j = 0; j < elements - i - 1; j++) {
-        if (s) {
-          s--;
-          continue;
-        }
+					generateBars();
+					highlightBar(i);
+					highlightBar(j);
+					await delay();
+				}
+			}
+		}
+	} else if (algorithm === "bubble") {
+		for (let i = 0; i < elements; i++) {
+			for (let j = 0; j < elements - i - 1; j++) {
+				if (s) {
+					s--;
+					continue;
+				}
 
-        if (!playing) return;
+				if (!playing) return;
 
-        step++;
+				step++;
 
-        if (numbers[j] > numbers[j + 1]) {
-          const temp = numbers[j];
-          numbers[j] = numbers[j + 1];
-          numbers[j + 1] = temp;
+				if (numbers[j] > numbers[j + 1]) {
+					const temp = numbers[j];
+					numbers[j] = numbers[j + 1];
+					numbers[j + 1] = temp;
 
-          generateBars();
-          highlightBar(j);
-          highlightBar(j + 1);
-          await delay();
-        }
-      }
-    }
-  } else if (algorithm === "insertion") {
-    let key;
+					generateBars();
+					highlightBar(j);
+					highlightBar(j + 1);
+					await delay();
+				}
+			}
+		}
+	} else if (algorithm === "insertion") {
+		let key;
 
-    for (let i = 1; i < elements; i++) {
-      if (s) {
-        s--;
-        continue;
-      }
-      if (!playing) return;
+		for (let i = 1; i < elements; i++) {
+			if (s) {
+				s--;
+				continue;
+			}
+			if (!playing) return;
 
-      key = numbers[i];
-      let j = i - 1;
+			key = numbers[i];
+			let j = i - 1;
 
-      while (key < numbers[j] && j >= 0) {
-        if (!playing) return;
+			while (key < numbers[j] && j >= 0) {
+				if (!playing) return;
 
-        numbers[j + 1] = numbers[j];
-        j--;
+				numbers[j + 1] = numbers[j];
+				j--;
 
-        generateBars();
-        highlightBar(i);
-        highlightBar(j + 1);
-        await delay();
-      }
+				generateBars();
+				highlightBar(i);
+				highlightBar(j + 1);
+				await delay();
+			}
 
-      numbers[j + 1] = key;
+			numbers[j + 1] = key;
 
-      generateBars();
-      highlightBar(i);
-      highlightBar(j + 1);
-      await delay();
+			generateBars();
+			highlightBar(i);
+			highlightBar(j + 1);
+			await delay();
 
-      step++;
-    }
-  }
+			step++;
+		}
+	}
 
-  stop();
-  generateBars();
+	stop();
+	generateBars();
 }
 
 function delay() {
-  return new Promise((resolve, reject) => setTimeout(() => resolve(), (10 - speed) * 1));
+	return new Promise((resolve, reject) => setTimeout(() => resolve(), (10 - speed) * 1));
 }
 
 generateNumbers();
